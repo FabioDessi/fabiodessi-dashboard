5237094d28b68d17cc3b4d780f58438adcf069da
Fabio Dessi <f.dessi@fabiodessi.dev>
2023-09-17 04:22:06 +0200

    feat: add 404 and 500 error handling

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ mongoose
 
 app.set('view engine', 'pug');
 
-// TODO: Add routes and page for 404 and 500s
 app.get('/', (req, res) => {
   res.render('pages/dashboard', { title: 'Homepage' });
 });
 
 app.use('/work-experiences', require('./routes/dashboardWorkExperiences'));
+
+// Catch-all 404 handler
+app.use((req, res, next) => {
+  res.status(404).send('Page Not Found');
+});
+
+// Error handling middleware (for 500 and other errors)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, console.log('Server started on port: ' + PORT));
